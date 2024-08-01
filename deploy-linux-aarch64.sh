ce52572e3bfa243f88e07c8324a51f2ec0ca4eb0
fujikang <35111141+fujikang@users.noreply.github.com>
2024-08-01 18:32:07 +0800

    Update and rename deploy-arm.sh to deploy-linux-aarch64.sh

--- deploy-linux-aarch64.sh
+++ deploy-linux-aarch64.sh
@@ -1,30 +1,36 @@
 #!/bin/bash
 set -ex
 
-WORKSPACE=/opt/als
-mkdir -p ${WORKSPACE}
-cd ${WORKSPACE}
+workspace=/opt/als
+mkdir -p ${workspace}
+cd ${workspace}
 
 # 下载, arm 机器替换 x86_64 为 aarch64
 OS_ARCH="aarch64"
 #下载部署包
-wget --no-check-certificate -qO "als-${OS_ARCH}.zip"  "https://github.com/fujikang/als/releases/download/als-${OS_ARCH}.zip"
+wget --no-check-certificate -qO "als-linux-${OS_ARCH}.zip"  "https://github.com/fujikang/als/releases/download/als-linux-${OS_ARCH}.zip"
 #解压部署包
-unzip -o "als-${OS_ARCH}.zip"
+unzip -o "als-linux-${OS_ARCH}.zip"
 
 # systemd service
-mv -v als-${OS_ARCH}.service /etc/systemd/system/als-${OS_ARCH}.service
+mv -v als-linux-${OS_ARCH}.service /etc/systemd/system/als-linux-${OS_ARCH}.service
+
+#处理可执行包
+mkdir -p /opt/als
+mv als-linux-${OS_ARCH}  /opt/als
+chmod +X /opt/als/als-linux-${OS_ARCH} 
+
 
 systemctl daemon-reload
 
 # 使用以下命令开机自启
-systemctl enable als-${OS_ARCH}
+systemctl enable als-linux-${OS_ARCH}
 
 # 启动
-systemctl start als-${OS_ARCH}
+systemctl start als-linux-${OS_ARCH}
 
 # 状态查看
-systemctl status als-${OS_ARCH}
+systemctl status als-linux-${OS_ARCH}
 
 
 
